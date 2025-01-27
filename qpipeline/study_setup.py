9463fdf6641db62b9539b4783df56054086ce55a
wmda <daniel.halls@nottingham.ac.uk>
2025-01-27 14:44:27 +0000

    working on parsing qunex create study output

--- qpipeline/study_setup.py
+++ qpipeline/study_setup.py
@@ -1,17 +1,154 @@
 import os
+from .utils import run_cmd
+import re
 
 
-def study_create(study_folder: str, qunex_con_image: str, sub_id: str) -> str:
-    return f"""
-qunex_container create_study \
---studyfolder="{study_folder}/{sub_id}" \
---bind="{study_folder}:{study_folder}" \
---container="{qunex_con_image}" 
-"""
+def study_create(study_folder: str, qunex_con_image: str, sub_id: str) -> list:
+    """
+    Function for the qunex create study command
+
+    Parameteres
+    -----------
+    study_folder: str
+        study folder path
+    qunex_con_image: str
+        qunex container path
+    sub_id: str
+        subject id
+
+    Returns
+    -------
+    list: list object
+        list of command
+    """
+
+    return [
+        f"""qunex_container create_study \\
+        --studyfolder={study_folder}/{sub_id}\\
+        --bind={study_folder}:{study_folder}\\
+        --container={qunex_con_image}
+        """
+    ]
 
 
-def set_up_qunex_study(args):
-    qunex_con_image = os.environ["QUNEXCONIMAGE"]
+def import_data(
+    study_folder: str, qunex_con_image: str, sub_id: str, raw_data: str
+) -> list:
+    """
+    Function for the qunex import bids command
+
+    Parameteres
+    -----------
+    study_folder: str
+        study folder path
+    qunex_con_image: str
+        qunex container path
+    sub_id: str
+        subject id
+
+    Returns
+    -------
+    list: list object
+        list of command
+    """
+
+    return [
+        f"""qunex_container import_bids \\
+    --sessionsfolder={study_folder}/{sub_id}/sessions \\
+    --inbox={raw_data} \\
+    --action=copy \\
+    --archive=leave \\
+    --overwrite=no \\
+    --bind={study_folder}:{study_folder},{raw_data}:{raw_data} \\
+    --container={qunex_con_image}
+    """
+    ]
+
+
+def map_files() -> dict:
+    """
+    Function to map files in qunex format.
+    Pipeline dynamically maps diffusion
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    dict: dictionary
+        dict of mapping files
+    """
+    return {"T1w": "T1w", "T2w": "T2w"}
+
+
+def map_scans(file_mapping: dict, label: str) -> str:
+    """
+    Function to map scans to correct quenx file name
+
+    Parameters
+    ----------
+    file_mapping: dict
+        dictionary of file mapping
+    label: str
+        label of scan name
+
+    Returns
+    -------
+    str: str object
+        string object of file mapping name
+    """
+    dwi_match = re.match(r"(dir[\d_]*-[AP]{2})_dwi", label)
+    if dwi_match:
+        return f"DWI:{dwi_match.group(1)}"
+    return file_mapping.get(label, label)
+
+
+def parse_output(output: str) -> None:
+    """
+    Function to parse through output
+    to create mapping file
+
+    Parameters
+    ----------
+    output: str
+        stdout of qunex command
+
+    Returns
+    -------
+    None
+    """
+    pattern = re.compile(r"---> linked (\d+\.nii\.gz) <-- sub-[^_]+_(.*)\.nii\.gz")
+    mapped_files = {}
+    file_mapping = map_files()
+    for match in pattern.finditer(output):
+        number = match.group(1).split(".")[0]
+        label = match.group(2)
+        mapped_files[number] = map_scans(file_mapping, label)
+    result = [f"{num} => {label}" for num, label in mapped_files.items()]
+    for item in result:
+        print(item)
+
+
+def set_up_qunex_study(args: dict) -> None:
+    """
+    Function for main set up quenx study
+
+    Parameters
+    ----------
+    args: dict
+        dictionary of cmd args
+
+    Returns
+    -------
+    None
+    """
+    qunex_con_image = os.environ["QUNEXCONIMAGE"].rstrip()
     create_study = study_create(args["study_folder"], qunex_con_image, args["id"])
-    print(create_study)
-    # run_cmd()
+    print("Creating study")
+    create_study_output = run_cmd(create_study)
+    data_importing = import_data(
+        args["study_folder"], qunex_con_image, args["id"], args["raw_data"]
+    )
+    import_data_output = output = run_cmd(data_importing)
+    parse_output(import_data_output["stdout"])
--- qpipeline/utils.py
+++ qpipeline/utils.py
@@ -1,5 +1,6 @@
 import subprocess
 import re
+import os
 
 
 def error_and_exit(
@@ -47,7 +48,8 @@ def run_cmd(command: list) -> dict:
         command
     """
     try:
-        run = subprocess.run(command, capture_output=True)
+        qpipe_env = os.environ.copy()
+        run = subprocess.run(command, capture_output=True, shell=True, env=qpipe_env)
     except subprocess.CalledProcessError as error:
         error_and_exit(False, f"Error in calling commnd due to: {error}")
     except KeyboardInterrupt:
@@ -57,6 +59,42 @@ def run_cmd(command: list) -> dict:
         key: value.decode("utf-8").strip() if isinstance(value, bytes) else value
         for key, value in vars(run).items()
     }
-    if output["stderr"]:
-        error_and_exit(False, f"Command failed due to {output['stderr']}")
+
     return output
+
+
+def write_to_file(
+    file_path: str, name: str, text: str, text_is_list: bool = False
+) -> bool:
+    """
+    Function to write to file.
+
+    Parameters
+    ----------
+    file_path: str
+        abosulte file path to
+        where file is created
+    name: str
+        name of file
+    text: str
+        string to add to file
+    text_is_list: bool
+        if text is actually a
+        list then will write to file
+
+    Returns
+    -------
+    bool: boolean
+        True if sucessful else
+        False
+    """
+    try:
+        with open(f"{file_path}/{name}", "w") as file:
+            if text_is_list:
+                file.writelines(text)
+            if not text_is_list:
+                file.write(text)
+    except Exception as e:
+        print(f"Unable to write to {file_path}/{name} due to :", e)
+        return False
+    return True
